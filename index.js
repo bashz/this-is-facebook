ee16e30f45765859c3cb31ad018933689a535adc
bechir <bechir@accessnow.org>
2015-12-06 20:39:09 +0100

    release 0.2.0

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var self = require('sdk/self');
 var tabs = require("sdk/tabs");
+var urls = require("sdk/url");
+var faceURL = require("sdk/simple-prefs").prefs.faceUrl;
+require("sdk/simple-prefs").on("faceUrl", imageChanged);
 var { attach, detach } = require('sdk/content/mod');
 var { Style } = require('sdk/stylesheet/style');
 var { ToggleButton } = require("sdk/ui/button/toggle");
 var facebookTabs = [];
 
+if(!urls.isValidURI(faceURL))
+    faceURL = self.data.url("images/default.gif");
+
 var style = Style({
     source: '.fbPhotosPhotoTagboxBase {'
             + 'background-size: 100% 110%;'
             + 'background-position: 20% 70%;'
-            + 'background-image: url(' + self.data.url("images/default.gif") + ');'
+            + 'background-image: url(' + faceURL + ');'
             + '}'
 });
 
@@ -17,11 +23,9 @@ var button = ToggleButton({
     id: "tif",
     label: "This Is Facebook",
     icon: {
-      "32": self.data.url("images/icons/icon-tif-32.png"),
-      "64": self.data.url("images/icons/icon-tif-64.png")
+        "32": self.data.url("images/icons/icon-tif-32.png"),
+        "64": self.data.url("images/icons/icon-tif-64.png")
     },
-    badge: 0,
-    badgeColor: "#00AAAA",
     onChange: onOff
 });
 
@@ -47,8 +51,29 @@ function onOff(state) {
     }
 }
 
+function imageChanged() {
+    var faceURL = require("sdk/simple-prefs").prefs.faceUrl;
+    if(!urls.isValidURI(faceURL))
+        faceURL = self.data.url("images/default.gif");
+    
+    for (var i = 0; i < facebookTabs.length; i++) {
+        detach(style, facebookTabs[i]);
+    }
+    style = Style({
+        source: '.fbPhotosPhotoTagboxBase {'
+                + 'background-size: 100% 110%;'
+                + 'background-position: 20% 70%;'
+                + 'background-image: url(' + faceURL + ');'
+                + '}'
+    });
+    for(var i = 0; i < facebookTabs.length; i++){
+        if(button.state(facebookTabs[i]).checked){
+            attach(style, facebookTabs[i]);
+        }
+    }
+}
+
 function isFacebookTab(uri){
-    var urls = require("sdk/url");
     if(urls.isValidURI(uri)){
         var url = urls.URL(uri);
         var splitedURL = url.hostname.split('.');
