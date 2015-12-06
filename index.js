9d861906937455528ef89685fc3556b5431f3849
bechir <bechir@accessnow.org>
2015-12-06 16:24:12 +0100

    adding 'on / off' functionaity

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 var self = require('sdk/self');
-var pageMod = require("sdk/page-mod");
+var tabs = require("sdk/tabs");
+var { attach, detach } = require('sdk/content/mod');
+var { Style } = require('sdk/stylesheet/style');
+var { ToggleButton } = require("sdk/ui/button/toggle");
+var facebookTabs = [];
 
-pageMod.PageMod({
-    include: "*.facebook.com",
-    contentStyleFile: self.data.url("css/default.css")
+var style = Style({
+    source: '.fbPhotosPhotoTagboxBase {'
+        +'background-size: 100% 110%;'
+        +'background-position: 20% 70%;'
+        +'background-image: url('+self.data.url("images/default.gif")+');'
+        +'}'
 });
+
+var button = ToggleButton({
+    id: "tif",
+    label: "This Is Facebook",
+    icon: {
+      "32": self.data.url("images/icons/icon-tif-32.png"),
+      "64": self.data.url("images/icons/icon-tif-64.png")
+    },
+    badge: 0,
+    badgeColor: "#00AAAA",
+    onChange: onOff
+});
+
+tabs.on('ready', function onOpen(tab) {
+    if(isFacebookTab(tab.url)){
+        if(button.state(tab).checked){
+            attach(style, tab);
+        }
+        facebookTabs.push(tab);
+    }
+});
+
+function onOff(state) {
+    if (state.checked) {
+        for(var i = 0; i < facebookTabs.length; i++){
+            attach(style, facebookTabs[i]);
+        }
+    }
+    else {
+        for(var i = 0; i < facebookTabs.length; i++){
+            detach(style, facebookTabs[i]);
+        }
+    }
+}
+
+function isFacebookTab(uri){
+    var urls = require("sdk/url");
+    if(urls.isValidURI(uri)){
+        var url = urls.URL(uri);
+        var splitedURL = url.hostname.split('.');
+        if (splitedURL.slice(splitedURL.length - 2, splitedURL.length).join('.') === 'facebook.com')
+            return true;
+        else
+            return false;
+    }else 
+        return false;
+}
